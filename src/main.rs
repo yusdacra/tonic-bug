e9d59bbb8db9c1278e746730d6efcfc72c1e9480
Yusuf Bera Ertan <y.bera003.06@protonmail.com>
2021-01-21 00:23:44 +0300

    refactor: clean up example code

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,35 @@
-mod phone {
-    tonic::include_proto!("grpc.testing");
-}
+tonic::include_proto!("grpc.testing");
 
-use flume::{Receiver, Sender};
-use tonic::{transport::Channel, Request, Response};
+use tonic::transport::Channel;
 
-type PhoneService = phone::phone_client::PhoneClient<Channel>;
+type PhoneService = phone_client::PhoneClient<Channel>;
 
 #[tokio::main]
 async fn main() {
     let mut client = PhoneService::connect("http://localhost:50051")
         .await
         .expect("aaa");
+
     let (tx, rx) = flume::unbounded();
 
-    // Uncommenting this will make it work fine
-    /*tx.send(phone::StreamCallRequest {
+    /* Uncommenting this will make it work fine
+    tx.send(StreamCallRequest {
         phone_number: "555".to_string(),
     })
-    .expect("aa");*/
+    .expect("aa");
+    */
 
     println!("before stream");
+
     let mut calls = client
         .stream_call(rx.into_stream())
         .await
         .expect("aa")
         .into_inner();
+
     println!("after stream");
 
-    tx.send(phone::StreamCallRequest {
+    tx.send(StreamCallRequest {
         phone_number: "444".to_string(),
     })
     .expect("aa");
